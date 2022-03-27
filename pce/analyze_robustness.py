39d41e0734f7c5475063662719297e712591c5f2
Federico <federico.sangati@gmail.com>
2022-03-27 11:59:44 +0900

    multi processing in robustness

--- pce/analyze_robustness.py
+++ pce/analyze_robustness.py
@@ -10,25 +10,42 @@ import os
 from tqdm import tqdm
 from pce.run_from_dir import run_simulation_from_dir
 import json
+from joblib import Parallel, delayed
 
-def test_robustness_seeds(base_dir, random_seed=123, num_trials=10, aggregation_function='MIN'):
-
-    seed_dirs = sorted([d for d in os.listdir(base_dir) if d.startswith('seed_')])
-    seed_pef = {}
-    for seed_str in tqdm(seed_dirs):
-        exp_dir = os.path.join(base_dir, seed_str)
-        evo_files = sorted([f for f in os.listdir(exp_dir) if 'evo_' in f])
-        if len(evo_files)==0:
-            print('No evo file, skipping...')
-            continue
-        _, _, data_record = run_simulation_from_dir(
+def test_robustness_single(base_dir, seed_str, random_seed, num_trials, aggregation_function):
+    exp_dir = os.path.join(base_dir, seed_str)
+    evo_files = sorted([f for f in os.listdir(exp_dir) if 'evo_' in f])
+    if len(evo_files)==0:
+        return -1
+    _, _, data_record = run_simulation_from_dir(
             exp_dir, quiet=True,
             random_seed=random_seed, 
             num_trials=num_trials, 
             aggregation_function=aggregation_function
         )
-        seed_pef[seed_str] = float(data_record['performance'])
-    seed_pef = {k:v for k,v in sorted(seed_pef.items(), key=lambda x: -x[1])}
+    perf = float(data_record['performance'])
+    return perf
+
+def test_robustness_seeds(
+    base_dir, random_seed=123, num_trials=10, aggregation_function='MIN',
+    num_cores=1):
+
+    seed_dirs = sorted([d for d in os.listdir(base_dir) if d.startswith('seed_')])
+
+    if num_cores == 1:
+        # single core                
+        perf = [
+            test_robustness_single(base_dir, seed_str, random_seed, num_trials, aggregation_function)
+            for seed_str in tqdm(seed_dirs)
+        ]
+    else:
+        # run parallel job            
+        perf = Parallel(n_jobs=num_cores)(
+            delayed(test_robustness_single)(base_dir, seed_str, random_seed, num_trials, aggregation_function) \
+            for seed_str in tqdm(seed_dirs)
+        )
+
+    seed_pef = {k:v for k,v in sorted(zip(seed_dirs, perf), key=lambda x: -x[1])}
     print(json.dumps(seed_pef, indent=3))
 
     out_file = os.path.join(base_dir, 'robustness.json')
@@ -48,9 +65,13 @@ if __name__ == "__main__":
     )
 
     parser.add_argument('--dir', type=str, required=True, help='Directory path')
+    parser.add_argument('--random_seed', type=int, default=123, help='Overriding sim random seed')
+    parser.add_argument('--num_cores', type=int, default=1, help='Number of cores')
 
     args = parser.parse_args()
 
     test_robustness_seeds(
         base_dir=args.dir, 
+        random_seed=args.random_seed,
+        num_cores=args.num_cores
     )
